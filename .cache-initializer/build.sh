268d5c98a69c1e356a2062bfcb42991f21355ff8
dmitrievanthony <dmitrievanthony@gmail.com>
2018-08-06 07:57:07 +0000

    Update

--- .cache-initializer/build.sh
+++ .cache-initializer/build.sh
@@ -2,12 +2,14 @@
 
 SCRIPT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
 TMP_DIR="tmp"
+IGNITE_TMP_DIR="ignite-tmp"
 
 cd ${SCRIPT_PATH}
 
 . ../.common.sh
 . ../env.sh
 
+fetch_and_build ${IGNITE_URL} ${IGNITE_BRANCH} ${IGNITE_TMP_DIR}
 fetch_and_build ${CACHE_INIT_URL} ${CACHE_INIT_BRANCH} ${TMP_DIR}
 
 VERSION=`ls ${TMP_DIR}/target/ignite-test-cache-*-jar-with-dependencies.jar \
@@ -16,3 +18,4 @@ echo Cache initializer version: ${VERSION} && \
 cp ${TMP_DIR}/target/ignite-test-cache-${VERSION}-jar-with-dependencies.jar ./cache-initializer.jar && \
       
 rm -rf ${TMP_DIR}
+rm -rf ${IGNITE_TMP_DIR}
